1a3c3032656d02f3a881ecd65caae41febe8585e
Your Name <ahmet.dikbayir@gmail.com>
2024-03-08 23:49:12 +0300

    userService updated

--- src/library/User.java
+++ src/library/User.java
@@ -15,4 +15,36 @@ public class User {
         this.email = email;
         this.user_tel = user_tel;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUser_tel() {
+        return user_tel;
+    }
+
+    public void setUser_tel(String user_tel) {
+        this.user_tel = user_tel;
+    }
 }
