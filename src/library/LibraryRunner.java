e175cc8c279dd0edf7c56602c75ba712bf387fd9
Your Name <ahmet.dikbayir@gmail.com>
2024-03-09 00:27:52 +0300

    borrow book and giveBackBook method created and updated

--- src/library/LibraryRunner.java
+++ src/library/LibraryRunner.java
@@ -8,17 +8,37 @@ public class LibraryRunner {
         library.book1.setBookName("Denizler Altında 20Bin Fersah");
         System.out.println(library.book1.getBookName());
 
+
+        Book book1 = new Book("Denizler Altında 20Bin Fersah", "Jules Verne", 2012, 256);
+
         Book book2 = new Book();
         book2.setBookName(DataBank.bookName.get(0));
         System.out.println(book2.getBookName());
 
         UserService userService = new UserService();
 
-        System.out.println(userService.user);
+        UserService userService1 = new UserService(new User("Ahmet", "Dikbayir", "a.dikbayir@gmail.com", "05456960914"));
+
+
+        System.out.println("--------FIRST----------");
+        System.out.println(userService.getUser());
+        System.out.println(userService1.getUser());
+        System.out.println("------------------");
+
 
         userService.borrowBook(book2);
+        userService1.borrowBook(book1);
 
-        System.out.println(userService.user);
+        System.out.println("--------SECOND----------");
+        System.out.println(userService.getUser());
+        System.out.println(userService1.getUser());
+        System.out.println("------------------");
+        userService.giveBackBook(book2);
+        userService1.giveBackBook(book1);
+
+        System.out.println("--------THIRD----------");
+        System.out.println(userService.getUser());
+        System.out.println(userService1.getUser());
 
 
     }
--- src/library/UserService.java
+++ src/library/UserService.java
@@ -2,9 +2,31 @@ package library;
 
 public class UserService {
 
-    User user = new User();
+    private User user = new User();
+
+    public UserService() {
+    }
+
+    public UserService(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     public void borrowBook(Book book){
         user.setBook(book);
+    }
+
+    public void giveBackBook(Book book){
+
+        if(book.getBookName().equalsIgnoreCase(user.getBook().getBookName())){
+            user.setBook(null);
+        }
     }
 }
